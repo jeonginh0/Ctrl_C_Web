64557b077c94a2aa2bb7d17be7a511ad22e02add
jeonginh0 <stitch010913@gmail.com>
2025-03-12 10:48:15 +0900

    Add - New Project

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,14 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-
-    return config;
-  },
+  /* config options here */
+  reactStrictMode: true,
 };
 
 export default nextConfig;
