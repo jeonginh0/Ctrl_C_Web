2a5828f3226b39f68fc31ce913e68b83f4798525
jeonginh0 <stitch010913@gmail.com>
2025-03-13 21:38:25 +0900

    Modify - setting modify

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  exportTrailingSlash: true,
+  output: 'export'
 };
 
 export default nextConfig;
