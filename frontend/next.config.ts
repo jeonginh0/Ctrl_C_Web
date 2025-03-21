01c53a8d7a50d72f2b463d90ae58dc7d29de2a03
jeonginh0 <stitch010913@gmail.com>
2025-03-21 18:16:50 +0900

    Modify - config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-  exportTrailingSlash: true,
+  trailingSlash: true,
+  output: 'export',  // next export 대신 이 옵션 사용
+  images: {
+    unoptimized: true, // Disable image optimization
+  },
 };
 
 export default nextConfig;
