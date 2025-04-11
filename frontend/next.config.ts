ab183c71481c9ea8e840f3f22771ea69af28be2f
jeonginh0 <stitch010913@gmail.com>
2025-04-11 16:47:01 +0900

    Modify - config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,13 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  trailingSlash: true,
   output: 'export',
   images: {
     unoptimized: true, // Disable image optimization
   },
-  assetPrefix: './',  // 👈 export 시 상대경로로 설정
-  basePath: '',
 };
 
 export default nextConfig;
