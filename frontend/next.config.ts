a5d51f4942346dd6c127f915be5d2e704f2294d0
jeonginh0 <stitch010913@gmail.com>
2025-04-13 00:46:56 +0900

    Modify - Docker setting

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
   trailingSlash: true,
   images: {
-    unoptimized: true, // Disable image optimization
+    unoptimized: true,
   },
 };
 
