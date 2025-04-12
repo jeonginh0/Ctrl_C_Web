a9b717007f680cc6cf394cf5acd87bb2c05c37b2
jeonginh0 <stitch010913@gmail.com>
2025-04-12 23:43:08 +0900

    Add - Docker 설정

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,42 +6,26 @@ import { join } from 'path';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { OcrModule } from './ocr/ocr.module';
-import { OcrService } from './ocr/ocr.service';
-import { OcrResult, OcrResultSchema } from './ocr/entity/ocr-result.schema';
 import { AnalysisModule } from './analysis/analysis.module';
 import { ChatRoomModule } from './chatroom/chatroom.module';
 import { ConversationModule } from './conversation/conversation.module';
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', '..', 'frontend', 'out'),
-      serveRoot: '/',
-    }),
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', '..', 'frontend', 'out', '_next'),
-      serveRoot: '/_next/',
-    }),
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'uploads'), // 업로드된 파일이 저장될 폴더 경로
-      serveRoot: '/uploads', // 클라이언트에서 접근할 수 있는 경로
-    }),
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
     }),
-    MulterModule.register({
-      dest: join(__dirname, '..', 'uploads'), // 절대 경로로 설정
-    }),
     MongooseModule.forRoot(process.env.MONGO_URI as string),
-    MongooseModule.forFeature([{ name: OcrResult.name, schema: OcrResultSchema }]),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
+      serveRoot: '/uploads',
+    }),
     AuthModule,
     OcrModule,
     AnalysisModule,
     ChatRoomModule,
-    ConversationModule
+    ConversationModule,
   ],
-  providers: [OcrService],
-  exports: [OcrService],
 })
 export class AppModule {}
