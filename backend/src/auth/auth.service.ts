b434c6cc41fc9a61f8622dd5edf675181d05e5e1
jeonginh0 <stitch010913@gmail.com>
2025-04-14 13:49:38 +0900

    Modify - env

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -223,13 +223,13 @@ export class AuthService {
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: process.env.GMAIL_USER,
-            pass: process.env.GMAIL_APP_PASSWORD
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASSWORD
         }
     });
 
     await transporter.sendMail({
-        from: process.env.GMAIL_USER,
+        from: process.env.EMAIL_USER,
         to: email,
         subject: '[Renalyze] 임시 비밀번호 발급',
         text: `
